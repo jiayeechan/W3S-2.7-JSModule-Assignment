5248b9679814d096740689dce333385c5a0b3dff
Jia Yee Chan <jiayee@ndnco.com.au>
2023-10-02 13:54:07 +1100

    set up package and installed convert-excel-to-json module

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const excelToJson = require('convert-excel-to-json');
+const fs = require('fs');
+ 
+const result = excelToJson({
+    source: fs.readFileSync('./data.xlsx') // fs.readFileSync return a Buffer
+});
+
+console.log(result);
